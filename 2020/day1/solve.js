e059acab0f20ccd6979ac6eb7d205928e2cf4edc
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-12-07 17:04:39 +0200

    Update solve.js

--- 2020/day1/solve.js
+++ 2020/day1/solve.js
@@ -1,1 +1,2 @@
 ``.trim().split('\n').map((n) => parseInt(n, 10)).map((x, i, list) => list.find((y) => x+y == 2020)*x).find((v) => !isNaN(v))
+``.trim().split('\n').map((n) => parseInt(n, 10)).map((x, i, list) => list.map((y) => list.find((z) => x+y+z == 2020)*y*x).find((v) => !isNaN(v))).filter((a) => a).pop()
