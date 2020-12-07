b2e97bbfbc718482a648cebbf7c17791fecbeeeb
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-12-07 19:08:44 +0200

    Update solve.js

--- 2020/day3/solve.js
+++ 2020/day3/solve.js
@@ -10,3 +10,24 @@ while((pos = adv())[1] < mapping.length) {
   trees += mapping[y][x] == '#' ? 1 : 0
 }
 console.log(trees)
+
+
+
+const mapping = ``.trim().split('\n')
+
+const slope = ([x, y]) => {
+	let pos = [0, 0]
+	return () => pos = [(pos[0]+x)%31, pos[1]+y]
+}
+
+const countTrees = (adv) => {
+	let pos = [0, 0]
+	let trees = 0	
+	while((pos = adv())[1] < mapping.length) {
+		const [x, y] = pos
+	    trees += mapping[y][x] == '#' ? 1 : 0
+	}
+	return trees
+}
+
+[[1,1], [3, 1], [5, 1], [7, 1], [1, 2]].reduce((sum, slp) => {return sum*countTrees(slope(slp))}, 1)
