f9862d04de1423fe8ca4470aeb8e5580e59e39de
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-12-07 18:04:43 +0200

    Update solve.js

--- 2020/day2/solve.js
+++ 2020/day2/solve.js
@@ -2,3 +2,9 @@
 	const [len, letter, str] = l.split(' ');
 	return str.replace(new RegExp(`[^${letter[0]}]+`, 'g'), '').match(new RegExp(`^\\w{${len.replace('-', ',')}}$`))
 }).filter(a=> a).length
+
+``.trim().split('\n').map(l => { 
+	const [len, letter, str] = l.split(' ');
+	const [x, y] = len.split('-')
+	return str[x-1] == letter[0] ^ str[y-1] == letter[0]
+}).reduce((t,c) => t+c)
