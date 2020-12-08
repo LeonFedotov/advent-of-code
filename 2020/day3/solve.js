6d03a42173c6b4bc1ea886e0d38db528b833dffe
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-12-08 08:25:14 +0200

    Update solve.js

--- 2020/day3/solve.js
+++ 2020/day3/solve.js
@@ -1,15 +1,3 @@
-let adv = (function(){
-	let pos = [0, 0]
-	return () => pos = [(pos[0]+3)%31, pos[1]+1]
-}())
-let pos = [0, 0]
-let trees = 0
-let mapping = input.trim().split('\n')
-while((pos = adv())[1] < mapping.length) {
-	const [x, y] = pos
-  trees += mapping[y][x] == '#' ? 1 : 0
-}
-console.log(trees)
 
 
 
@@ -29,5 +17,5 @@ const countTrees = (adv) => {
 	}
 	return trees
 }
-
+countTrees([3,1];
 [[1,1], [3, 1], [5, 1], [7, 1], [1, 2]].reduce((sum, slp) => {return sum*countTrees(slope(slp))}, 1)
