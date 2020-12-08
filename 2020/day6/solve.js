974946f6461e20e8ad81e9f03d0e9ca6e07e0288
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-12-08 18:10:57 +0200

    Update solve.js

--- 2020/day6/solve.js
+++ 2020/day6/solve.js
@@ -1,1 +1,2 @@
 ``.trim().split('\n\n').map(g => g.split('\n').join('').split('').filter((v, i, s) => s.indexOf(v) == i).length).reduce((a,b) => a+b)
+``.trim().split('\n\n').map(g => g.split('\n').join('').split('').filter((v, i, s) => s.indexOf(v) == i).filter((l) => g.split('\n').every((u) => u.indexOf(l) > -1)).length).reduce((a,b) => a+b)
