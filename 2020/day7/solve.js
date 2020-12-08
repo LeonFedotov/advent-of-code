92b8d560ccd52be1a0474117c625d6b79e5382cb
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-12-08 21:00:52 +0200

    Update solve.js

--- 2020/day7/solve.js
+++ 2020/day7/solve.js
@@ -24,3 +24,28 @@ a
 	.split(',')
 	.filter((v, i, self) => self.indexOf(v) == i && v != 'shiny gold')
 	.length
+
+
+``.replace(/(\s*bag\.|\s*bags\.|\s*bags|\s*bag)/g,'')
+	.replace(/ contain /g,', ')
+	.split('\n')
+	.filter((r) => r.indexOf('no other') == -1)
+	.map((r) => r.split(', '))
+	.reduce((map, [parent, ...children], index, list) => {
+		map[parent] = children
+		
+		if(index == list.length-1) {
+			const tracePath = (child) => {
+				const [count, ...name] = child.split(' ')
+				const c = parseInt(count, 10)
+				const n = name.join(' ')
+
+
+				return !map[n] ? c : c+c*map[n].reduce((a, cur) => a + tracePath(cur), 0)
+			}
+			return tracePath('1 shiny gold')
+		} else {
+			return map
+		}
+		
+	}, {})-1
