e142027d39543e086cfa89b41308bffe98173e89
LeonFedotov <fedotov.leon@gmail.com>
2024-11-30 00:16:01 +0200

    day7-1-star

--- 2023/day7/solve-part2.js
+++ 2023/day7/solve-part2.js
@@ -1,15 +1,26 @@
 const _ = require('lodash')
 const { readFileSync } = require('fs')
 const log = (v) => console.dir(v, { depth: null })
-const rank = [
-  'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'
-].reduce((acc, value, index, input) => ({ ...acc, [value]: input.length - index }), {});
+
+const rank = [ 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'].reduce((acc, value, index, input) => ({ ...acc, [value]: input.length - index }), {});
 
 
 _.chain(readFileSync('./input'))
   .split('\n')
   .map((line) => line.split(' '))
   .map(([hand, bid]) => [hand, bid, _.chain(hand).split('').groupBy().values().sortBy((g) => -g.length).value()])
+  .tap(log)
+  .map(([hand, bid, cardGrouping]) => {
+    if(hand.search('J') > -1) {
+      const jIndex = cardGrouping.findIndex(([f]) => f == 'J')
+      const prev = cardGrouping[0]
+
+      cardGrouping.find(([f]) => f=='J').forEach((v, i) => prev.push(prev[0]))
+
+    }
+    return [hand, bid, cardGrouping.filter(([f]) => f!='J')]
+  })
+  .tap(log)
   .map(([hand, bid, cardGrouping]) => [hand, bid, (
     cardGrouping.length == 1 ? 6 :
     cardGrouping.length == 2 ? cardGrouping[0].length == 4 ? 5 : 4 :
@@ -19,11 +30,10 @@ _.chain(readFileSync('./input'))
   )])
   .sortBy(([,, rank]) => rank)
   .groupBy(([,, rank]) => rank)
-  .reduce((acc, value) => [...acc, ...value.sort(([handA], [handB]) =>
-    handA.split('').reduce((acc, value, index) => acc || (rank[value] - rank[handB[index]]), 0)
-  )], [])
+  .reduce((acc, value) => [...acc, ...value.sort(([handA], [handB]) => handA.split('').reduce((acc, value, index) => acc || (rank[value] - rank[handB[index]]), 0))], [])
+  .tap(log)
   .map(([hand, bid], index) => [hand, bid, index+1])
-  .map(([hand, bid, rank]) => rank*bid)
+  .map(([, bid, rank]) => rank*bid)
   .sum()
   .tap(log)
   .value()
