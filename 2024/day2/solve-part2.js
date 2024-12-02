d8fd30e36cabf8107b39a9952b84bc1e8701ee74
Leon Fedotov <fedotov.leon@gmail.com>
2024-12-02 10:31:28 +0200

    to spliced instead of filter

--- 2024/day2/solve-part2.js
+++ 2024/day2/solve-part2.js
@@ -8,7 +8,7 @@ _
   .trim()
   .split('\n')
   .map(report => report.split(' ').map(Number))
-  .map(report => [report, ...report.map((level, index) => report.filter((l, i) => i !== index))])
+  .map(report => [report, ...report.map((level, index) => report.toSpliced(index, 1))])
   .filter((mlevels) => mlevels.some((levels) =>
       levels.slice(1).every((level, index) => level > levels[index] && level <= levels[index]+3) ||
       levels.slice(1).every((level, index) => level < levels[index] && level >= levels[index]-3)
