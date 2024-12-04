c259ba930be52a1a36a48458e81cc5ebde1ee0a0
LeonFedotov <fedotov.leon@gmail.com>
2024-12-04 11:21:10 +0200

    refactor

--- 2024/day4/solve-part1.js
+++ 2024/day4/solve-part1.js
@@ -2,27 +2,23 @@ const _ = require('lodash')
 const { readFileSync } = require('fs')
 
 const log = (v) => console.dir(v, { depth: null })
-//XMAS
-
-const countAraound = (map, x, y) => (
-    map[y][x] != 'X' ? 0 : (
-      ((map[y]?.[x+1] == 'M' && map[y]?.[x+2] == 'A' && map[y]?.[x+3] == 'S') ? 1 : 0) +
-      ((map[y]?.[x-1] == 'M' && map[y]?.[x-2] == 'A' && map[y]?.[x-3] == 'S') ? 1 : 0) +
-      ((map[y-1]?.[x] == 'M' && map[y-2]?.[x] == 'A' && map[y-3]?.[x] == 'S') ? 1 : 0) +
-      ((map[y+1]?.[x] == 'M' && map[y+2]?.[x] == 'A' && map[y+3]?.[x] == 'S') ? 1 : 0) +
-
-      ((map[y+1]?.[x+1] == 'M' && map[y+2]?.[x+2] == 'A' && map[y+3]?.[x+3] == 'S') ? 1 : 0) +
-      ((map[y-1]?.[x-1] == 'M' && map[y-2]?.[x-2] == 'A' && map[y-3]?.[x-3] == 'S') ? 1 : 0) +
-      ((map[y+1]?.[x-1] == 'M' && map[y+2]?.[x-2] == 'A' && map[y+3]?.[x-3] == 'S') ? 1 : 0) +
-      ((map[y-1]?.[x+1] == 'M' && map[y-2]?.[x+2] == 'A' && map[y-3]?.[x+3] == 'S') ? 1 : 0)
-    )
-  )
 _
   .chain(readFileSync('./input'))
   .trim()
   .split('\n')
   .map((line) => line.split(''))
-  .map((line, y, letters) => line.map((_, x) => countAraound(letters, x, y)))
+  .map((line, y, letters) => line.map((_, x) => (
+    letters[y][x] != 'X' ? 0 : (
+      ((letters[y]?.[x + 1] == 'M' && letters[y]?.[x + 2] == 'A' && letters[y]?.[x + 3] == 'S') ? 1 : 0) +
+      ((letters[y]?.[x - 1] == 'M' && letters[y]?.[x - 2] == 'A' && letters[y]?.[x - 3] == 'S') ? 1 : 0) +
+      ((letters[y - 1]?.[x] == 'M' && letters[y - 2]?.[x] == 'A' && letters[y - 3]?.[x] == 'S') ? 1 : 0) +
+      ((letters[y + 1]?.[x] == 'M' && letters[y + 2]?.[x] == 'A' && letters[y + 3]?.[x] == 'S') ? 1 : 0) +
+      ((letters[y + 1]?.[x + 1] == 'M' && letters[y + 2]?.[x + 2] == 'A' && letters[y + 3]?.[x + 3] == 'S') ? 1 : 0) +
+      ((letters[y - 1]?.[x - 1] == 'M' && letters[y - 2]?.[x - 2] == 'A' && letters[y - 3]?.[x - 3] == 'S') ? 1 : 0) +
+      ((letters[y + 1]?.[x - 1] == 'M' && letters[y + 2]?.[x - 2] == 'A' && letters[y + 3]?.[x - 3] == 'S') ? 1 : 0) +
+      ((letters[y - 1]?.[x + 1] == 'M' && letters[y - 2]?.[x + 2] == 'A' && letters[y - 3]?.[x + 3] == 'S') ? 1 : 0)
+    )
+  )))
   .flatten()
   .sum()
   .tap(log)
--- 2024/day4/solve-part2.js
+++ 2024/day4/solve-part2.js
@@ -2,20 +2,18 @@ const _ = require('lodash')
 const { readFileSync } = require('fs')
 
 const log = (v) => console.dir(v, { depth: null })
-//XMAS
-
-const countAraound = (map, x, y) => (
-    map[y][x] == 'A' &&
-    ['MAS', 'SAM'].includes(map[y-1]?.[x-1]+map[y][x]+map[y+1]?.[x+1]) &&
-    ['MAS', 'SAM'].includes(map[y-1]?.[x+1]+map[y][x]+map[y+1]?.[x-1])
-  ) ? 1 : 0
-
 _
   .chain(readFileSync('./input'))
   .trim()
   .split('\n')
   .map((line) => line.split(''))
-  .map((line, y, letters) => line.map((_, x) => countAraound(letters, x, y)))
+  .map((line, y, letters) => line.map((_, x) => (
+    (
+      letters[y][x] == 'A' &&
+      ['MAS', 'SAM'].includes(letters[y-1]?.[x-1]+letters[y][x]+letters[y+1]?.[x+1]) &&
+      ['MAS', 'SAM'].includes(letters[y-1]?.[x+1]+letters[y][x]+letters[y+1]?.[x-1])
+    ) ? 1 : 0
+  )))
   .flatten()
   .sum()
   .tap(log)
