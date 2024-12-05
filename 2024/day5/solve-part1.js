cc3845c55aa9e015b60ff87ff10bf4660d6f61dd
LeonFedotov <fedotov.leon@gmail.com>
2024-12-05 11:19:20 +0200

    day5

---
+++ 2024/day5/solve-part1.js
@@ -0,0 +1,28 @@
+const _ = require('lodash')
+const { readFileSync } = require('fs')
+
+const log = (v) => console.dir(v, { depth: null })
+
+const isLegalUpdate = (rules, update) =>
+  update.every((u, index) =>
+    index == 0 || !rules[u] || rules[u].every((r) => !update.slice(0, index).includes(r)))
+
+_
+  .chain(readFileSync('./input-short'))
+  .trim()
+  .split('\n\n')
+  .thru(([rules, updates]) => [
+    rules
+      .split('\n')
+      .map((rule) => rule.split('|').map(Number))
+      .reduce((rules, [key, value]) => ({
+        ...rules,
+        ...(!rules[key] ? { [key]: [value] } : { [key]: [...rules[key], value]})
+      }), {}),
+    updates.split('\n').map((u) => u.split(',').map(Number))
+  ])
+  .thru(([rules, updates]) => updates.filter((update) => isLegalUpdate(rules, update)))
+  .map((update) => update[Math.round(update.length/2)-1])
+  .sum()
+  .tap(log)
+  .value()
---
+++ 2024/day5/solve-part2.js
@@ -0,0 +1,34 @@
+const _ = require('lodash')
+const { readFileSync } = require('fs')
+
+const log = (v) => console.dir(v, { depth: null })
+
+const isLegalUpdate = (rules, update) =>
+  update.every((u, index) =>
+    index == 0 || !rules[u] || rules[u].every((r) => !update.slice(0, index).includes(r)))
+
+_
+  .chain(readFileSync('./input'))
+  .trim()
+  .split('\n\n')
+  .thru(([rules, updates]) => [
+    rules
+      .split('\n')
+      .map((rule) => rule.split('|').map(Number))
+      .reduce((rules, [key, value]) => ({
+        ...rules,
+        ...(!rules[key] ? { [key]: [value] } : { [key]: [...rules[key], value]})
+      }), {}),
+    updates.split('\n').map((u) => u.split(',').map(Number))
+  ])
+  .thru(([rules, updates]) => [rules, updates.filter((update) => !isLegalUpdate(rules, update))])
+  .thru(([rules, updates]) => updates.map((update) => update.reduce((acc, u, index) => (
+    !rules[u] || !index ? [...acc, u] : acc.reduce(([acc, rest], current) => (
+        !rules[u].includes(current) ? [[...acc, current], rest] : [acc, [...rest, current]]
+      ), [[], [u]])
+      .flat()
+  ), [])))
+  .map((update) => update[Math.round(update.length/2)-1])
+  .sum()
+  .tap(log)
+  .value()
