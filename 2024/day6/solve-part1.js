57c49e534cd21aac70b19c29b615d5a47840b6bc
LeonFedotov <fedotov.leon@gmail.com>
2024-12-06 12:31:05 +0200

    init day 6

---
+++ 2024/day6/solve-part1.js
@@ -0,0 +1,10 @@
+const _ = require('lodash')
+const { readFileSync } = require('fs')
+
+const log = (v) => console.dir(v, { depth: null })
+
+_
+  .chain(readFileSync('./input-short'))
+  .trim()
+  .tap(log)
+  .value()
